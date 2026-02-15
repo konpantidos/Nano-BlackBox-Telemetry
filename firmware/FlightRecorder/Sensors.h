a05af09ccbc22178126618072546d16b62b03bbe
konpantidos <kon.pantidos@gmail.com>
2026-02-15 13:20:27 +0200

    Update Sensors.h

--- firmware/FlightRecorder/Sensors.h
+++ firmware/FlightRecorder/Sensors.h
@@ -1,1 +1,20 @@
+#ifndef SENSORS_H
+#define SENSORS_H
 
+#include <Arduino.h>
+
+// Structure to hold all sensor data in one neat package
+struct FlightData {
+    float ax, ay, az;   // Acceleration (G)
+    float gx, gy, gz;   // Gyroscope (deg/s)
+    float altitude;     // Altitude (meters)
+    float temperature;  // Temperature (C)
+    unsigned long timestamp; // Time since boot (ms)
+};
+
+// Function declarations
+void initSensors();
+FlightData readSensors();
+void calibrateSensors(); // Optional: Call on startup to zero gyro
+
+#endif

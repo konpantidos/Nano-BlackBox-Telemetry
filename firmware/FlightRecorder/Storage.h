fce04aae7ae383d63fc8fd52802c897c6da3a1c3
konpantidos <kon.pantidos@gmail.com>
2026-02-15 13:21:39 +0200

    Update Storage.h

--- firmware/FlightRecorder/Storage.h
+++ firmware/FlightRecorder/Storage.h
@@ -1,1 +1,12 @@
+#ifndef STORAGE_H
+#define STORAGE_H
 
+#include <Arduino.h>
+#include "Sensors.h" // We need to know what FlightData looks like
+
+// Function declarations
+bool initStorage();
+void logData(FlightData data);
+void closeStorage(); // Safely save file on landing
+
+#endif
